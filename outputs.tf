462276050a6df9c442c37aa8545927c303f025cb
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-29 22:03:03 +0200

    Add outputs.

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "roles" {
+  description = "List of roles with attributes map."
+  value       = local.roles
+}
+
+output "policies" {
+  description = "List of policies with attributes map."
+  value       = local.policies
+}
